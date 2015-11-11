f5a942dc7a64387e8eab38eefc18efdc52c850d9
teampricing <team.pricing@aimsoftware.com>
2015-11-11 14:31:25 +0100

    Prepared project files

--- MahApps.Metro/Properties/AssemblyInfo.cs
+++ MahApps.Metro/Properties/AssemblyInfo.cs
@@ -19,4 +19,4 @@ using System.Windows.Markup;
 [assembly: AssemblyDescriptionAttribute("Toolkit for creating Metro styled WPF apps")]
 [assembly: AssemblyProductAttribute("MahApps.Metro 1.1.3-ALPHA")]
 [assembly: AssemblyCompany("MahApps")]
-[assembly: InternalsVisibleTo("Mahapps.Metro.Tests")]
+[assembly: InternalsVisibleTo("Mahapps.Metro.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001009734ff9171bad8559a6bf37f7227ed8268a700f7d0c1930b55a9d90ba5a60d3f0be12ba10c82938727e3abd01d76ba48dd061c5331ef42ce05dc08b242043211c24c9558b5884de6e283b2be482e9d27876ea532abb438d265ba4a315e551de02176b18c9ffe004741de54c6217d2557f0d38845f85cdaa5e3c10b051b1788c1")]
